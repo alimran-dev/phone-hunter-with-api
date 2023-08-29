7835b81d2df897221c8198c6981380b4db86a578
alimran-dev <alimran5air@gmail.com>
2023-08-29 16:12:43 +0600

    search text default value removed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const loadPhones = async (searchText = '13', isShowAll) => {
+const loadPhones = async (searchText, isShowAll) => {
   const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
   const data = await res.json();
   const phones = data.data;
